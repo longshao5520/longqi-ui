86eed6cbc15ca604d3304c77316e8697a4c722d1
Long Qi <longshao5520@outlook.com>
2022-09-24 06:33:27 +0000

    update

--- examples/app.vue
+++ examples/app.vue
@@ -1,107 +1,32 @@
 <template>
   <div>
-    <!--    <lq-form v-model="form" :option="options" style="width: 800px;"/>-->
-<!--    <LqUpload v-if="isRouterAlive" v-model:fileList="file" :option="fileOption"/>-->
-<!--    <LqUpload v-model:fileList="file" :option="fileOption1"/>-->
-    <LqUpload v-if="isRouterAlive" v-model:fileList="file" :option="fileOption2"/>
+    <lq-form v-model="form" :option="options" style="width: 800px;"/>
   </div>
 </template>
 
 <script lang="ts" setup>
-import {LqFormOptions,OptionsColumn} from 'longqi-ui/src/form/types'
-import {nextTick, provide, reactive, ref} from "vue";
+import {defineLqForm} from 'longqi-ui/src/form/types'
+import {reactive} from "vue";
 
-// 局部组件刷新
-const isRouterAlive = ref(true);
-const reload = () => {
-  // 删除组件
-  isRouterAlive.value = false;
-  nextTick(() => {
-    // 重新渲染组件
-    isRouterAlive.value = true;
-  });
-};
-provide("reload", reload);
+const form = reactive({})
 
-const file = reactive([{
-  name: 'food.jpeg',
-  url: 'http://file.cs08.cn/file/group1/M00/06/7D/rBABQWMamfuAHHcuAAJZakE_zgg707.jpg'
-}])
-const fileOption = reactive({
-  type: "upload",
-  action: "https://file.cs08.cn/file/uploadUtil",
-  // showFileList: false,
-  name: 'file',
-  drag: true,
-  propsHttp: {
-    name: 'fileName'
-  }
-} as OptionsColumn)
-const fileOption1 = reactive({
-  type: "uploadImg",
-  action: "https://file.cs08.cn/file/uploadUtil",
-  showFileList: false,
-  name: 'file',
-  propsHttp: {
-    name: 'fileName'
-  }
+const options = defineLqForm({
+  column: [{
+    label: "照片墙",
+    prop: "file",
+    type: "upload",
+    action: "https://file.cs08.cn/file/uploadUtil",
+    showFileList: false,
+    drag: true,
+    showThumbnail: true,
+    name: 'file',
+    value: [{name: 'food.jpeg', url: 'https://file.cs08.cn/file/group1/M00/06/7D/rBABQWMamfuAHHcuAAJZakE_zgg707.jpg'}],
+    propsHttp: {
+      url: "url",
+      name: "fileName",
+      res: "data",
+    },
+    tip: '1111',
+  }]
 })
-const fileOption2 = reactive({
-  type: "uploadImgCard",
-  action: "https://file.cs08.cn/file/uploadUtil",
-  // showFileList: false,
-  name: 'file',
-  propsHttp: {
-    name: 'fileName'
-  }
-})
-
-const uploadChange = () => {
-  console.log(fileOption2)
-}
-
-// const form = reactive({} as { file: string })
-//
-// const options = reactive({
-//   width: "400px",
-//   column: [{
-//     label: "照片墙",
-//     prop: "file",
-//     type: "uploadImgCard",
-//     action: "https://file.cs08.cn/file/uploadUtil",
-//     showFileList: false,
-//     name: 'file',
-//     // value: [{name: 'food.jpeg', url: 'http://file.cs08.cn/file/group1/M00/06/7D/rBABQWMamfuAHHcuAAJZakE_zgg707.jpg'}],
-//     // value: 'http://file.cs08.cn/file/group1/M00/06/7D/rBABQWMamfuAHHcuAAJZakE_zgg707.jpg',
-//     propsHttp: {
-//       url: "url",
-//       name: "fileName",
-//       res: "data",
-//     },
-//     // tip: '1111',
-//     // onSuccess: (response, uploadFile) => {
-//     //   console.log(response.data.url)
-//     //   form.file = response.data.url
-//     // }
-//   },{
-//     label: "附件",
-//     prop: "file1",
-//     type: "upload",
-//     action: "https://file.cs08.cn/file/uploadUtil",
-//     name: 'file',
-//     drag: true,
-//     value: [{name: 'food.jpeg', url: 'http://file.cs08.cn/file/group1/M00/06/7D/rBABQWMamfuAHHcuAAJZakE_zgg707.jpg'}],
-//     // value: 'http://file.cs08.cn/file/group1/M00/06/7D/rBABQWMamfuAHHcuAAJZakE_zgg707.jpg',
-//     propsHttp: {
-//       url: "url",
-//       name: "fileName",
-//       res: "data",
-//     },
-//     // tip: '1111',
-//     // onSuccess: (response, uploadFile) => {
-//     //   console.log(response.data.url)
-//     //   form.file = response.data.url
-//     // }
-//   }]
-// } as LqFormOptions)
 </script>
