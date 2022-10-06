7ec70a8ed62128c133419ff81eff0a5c60f30f8e
longshao5520 <longshao5520@outlook.com>
2022-10-06 15:55:32 +0800

    update form type Checkbox

---
+++ docs/docs/components/form/checkbox/border.vue
@@ -0,0 +1,38 @@
+<template>
+  <lq-form v-model="form" :option="options"></lq-form>
+</template>
+
+<script lang="ts">
+import {defineComponent, reactive} from "vue";
+import {defineLqForm} from "longqi-ui";
+
+
+export default defineComponent({
+  setup() {
+    const form = reactive({});
+
+    const options = defineLqForm({
+      column: [
+        {
+          label: '多选',
+          prop: 'checkbox',
+          type: 'checkbox',
+          border: true,
+          dicData: [{
+            label: '男',
+            value: 0
+          }, {
+            label: '女',
+            value: 1
+          }, {
+            label: '未知',
+            value: ''
+          }]
+        }
+      ]
+    });
+
+    return {form, options}
+  }
+});
+</script>
